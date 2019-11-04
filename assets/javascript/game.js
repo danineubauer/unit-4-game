edd3b0e3db19e659a55a845f88ab4fd290b2be99
Danielle <danineubauerr@gmail.com>
2019-11-03 23:48:50 -0800

    edits

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,8 +16,9 @@ $(document).ready(function() {
     //generate crystal count: 
 
     //display random count:    
-    $("#randomCount").text(randomNumber)
-    
+    $("#randomCount").text(randomCountNumber);
+
+
     //display wins/loses: 
     $("#wins").text(wins);
     $("#losses").text(losses); 
@@ -61,111 +62,23 @@ $(document).ready(function() {
         if (yourCount === randomCountNumber) { 
             wins++; 
             alert("You won!")
+            newGame();
+        } else if ( yourCount < randomCountNumber) {
+            $("#yourCount").text(yourCount);
+            console.log("your Count: ", yourCount);
+            console.log("random Count: ", randomCountNumber);
         } else {
             losses++;
+            newGame();
             alert("You lost! Your count didn't add up to the number") 
         }
     }
 
-    //create event listener for each crystal when clicked: 
-    //add up the user score
-    //check if user count equals the random count: 
-
-
+    function newGame() {
+        randomCountNumber = 0;
+        yourCount = 0; 
+        crystalRandomNumber();
+    }
 
 });
 
-
-
-//OLD CODE 
-
-// $(document).ready(function () {
-
-//     //Variables: 
-//     var num = [(Math.random() * 30).toFixed(1)] + 0;
-//     var wins = 0;
-//     var losses = 0;
-//     var userScore = 0;
-
-//     //Nodes: 
-//     $("#randomNumber").text(num);
-//     $("#wins").text(wins);
-//     $("#losses").text(losses);
-//     $("#userScore").text(userScore);
-
-//     //functions:
-
-//     var newNum = function() { 
-//         var num = [(Math.random() * 30).toFixed(1)] + 0;
-//         $("#randomNumber").text(num);
-//     }
-
-//     //New Game (reload page): 
-//     $('#newGame').click(function () {
-//         window.location.reload(true);
-//     });
-
-//     //reload page funtion: 
-//     var newGameFunc = function () {
-//         window.location.reload(true);
-//     };
-
-//     //if / else score function:
-//     var checkScore = function () {
-//         if (userScore.toFixed(2) === (num)) {
-//             console.log(userScore, num);
-//             wins++;
-//             $("#wins").text(wins); 
-//             console.log("wins", wins);            
-//             userScore = 0;  
-//             $("#userScore").text(userScore);   
-//             alert("Great Job! You paid your bill");
-//             newNum();
-//         } else if (userScore > num) {
-//             console.log(userScore, num);
-//             losses++;
-//             $("#losses").text(losses);
-//             console.log("loses", losses);
-//             userScore = 0; 
-//             $("#userScore").text(userScore);
-//             alert("you lose");
-//             newNum();
-//         };
-//     };
-
-//     //clicks on coins:
-//     $(".tenA").on("click", function (event) {
-//         userScore = userScore + 0.10;
-//         $("#userScore").text(userScore.toFixed(2));
-//         checkScore();
-//     });
-
-//     $(".half").on("click", function (event) {
-//         userScore = userScore + 0.50;
-//         $("#userScore").text(userScore.toFixed(2));
-//         checkScore();
-//     });
-
-//     $(".shek").on("click", function (event) {
-//         userScore = userScore + 1.00;
-//         $("#userScore").text(userScore.toFixed(2));
-//         checkScore();
-//     });
-
-//     $(".twoShek").on("click", function (event) {
-//         userScore = userScore + 2.00;
-//         $("#userScore").text(userScore.toFixed(2));
-//         checkScore();
-//     });
-
-//     $(".fiveShek").on("click", function (event) {
-//         userScore = userScore + 5.00;
-//         $("#userScore").text(userScore.toFixed(2));
-//     });
-
-//     $(".tenShek").on("click", function (event) {
-//         userScore = userScore + 10.00;
-//         $("#userScore").text(userScore.toFixed(2));
-//         checkScore();
-//     });
-// });
